bdb9e8cacab9d23180043bf0e7f5b1cdc22663b3
HamzahSk <168325326+HamzahSk@users.noreply.github.com>
2026-07-08 11:15:42 +0700

    Last Sync: 2026-07-08 11:15 (Mobile)

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // config.js
 module.exports = {
   // Ganti URL ini dengan website yang mau kamu scrape
-  targetUrl: ['https://z-library.sk/author/Yuuji']
+  targetUrls: ['https://z-library.sk/author/Yuuji']
 };
