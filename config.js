090954fa765d200d3b849b1aa6023bd0e146c433
HamzahSk <168325326+HamzahSk@users.noreply.github.com>
2026-07-08 12:15:13 +0700

    Last Sync: 2026-07-08 12:15 (Mobile)

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js
 module.exports = {
   // Ganti URL ini dengan website yang mau kamu scrape
-  targetUrls: ['https://z-library.sk/author/Yuuji']
+  targetUrls: ['https://vymanga.net/read/checkmate-r/2247308', 'https://z-library.sk/author/Yuuji']
 };
  
